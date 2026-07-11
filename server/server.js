210d6ee3d0a86dfd6890b86228af8b450acbe915
udaykora <129069866+udaykora@users.noreply.github.com>
2026-07-11 20:58:22 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -27,7 +27,7 @@ exports.helloWorld = onRequest((req, res) => {
   res.send("Hello World");
 });
 
-app.listen(5000, () => console.log('Server running on http://localhost:5000'));s
+// app.listen(5000, () => console.log('Server running on http://localhost:5000'));s
 
 
 // exports.api = functions.https.onRequest(app);
