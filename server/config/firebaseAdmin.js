d2af0e569d13af53d7ec248d5e38d36c403edc3b
udaykora <129069866+udaykora@users.noreply.github.com>
2026-07-11 20:56:47 +0530

    Update firebaseAdmin.js

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -4,10 +4,11 @@ const { getAuth } = require('firebase-admin/auth');
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 const app = admin.initializeApp({
-  credential: admin.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = getFirestore(app);
 const auth = getAuth(app);
+
 
 module.exports = { admin, db, auth, FieldValue };
